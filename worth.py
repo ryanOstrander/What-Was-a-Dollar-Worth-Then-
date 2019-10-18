862f0c6b35d0f77b82012663e729c394da0ab644
ryanOstrander <39414724+ryanOstrander@users.noreply.github.com>
2019-10-18 13:38:24 -0500

    Add files via upload

---
+++ worth.py
@@ -0,0 +1,65 @@
+import csv
+
+# reads in a CSV file for inflation data between 1960 and 2019
+def inflation_reader(dict):
+    # read the csv file in
+    with open('inflation_data.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+    
+        # sort values into a dictionary {key = year, value = amount}
+        for row in csv_reader:
+            dict[row['year']] = round(float(row['amount']), 2)
+
+# calculate total percent change from one year to another
+# includes error checking for year boundary condition
+def calculate_pct_change(orig, inc, dict):
+
+    # boundary condition
+    if ((orig >= 1960 or orig <= 2019) and (inc >= 1960 or inc <= 2019)):
+        _original = dict[str(orig)]
+        increase = dict[str(inc)] - _original
+        change = round((increase/_original) * 100.0, 2)
+    else:
+        return -999
+
+    return change
+
+def main():
+    # dictionary to store CSV information
+    inflation_dict = {}
+    inflation_reader(inflation_dict)
+
+    # prompt for user input
+    original = input("Enter a year: ")
+
+    # exit condition (input is -999)
+    while (original != -999):
+
+        # prompt for more information
+        new_number = input("Enter another year: ")
+        original_amount = input("Enter amount: ")
+
+        # calculate total percent change
+        change = calculate_pct_change(original, new_number, inflation_dict)
+        decimal_change = change / 100.0
+
+        # calculate total change between years for a given amount
+        total_dollar = (original_amount*decimal_change) + original_amount
+
+        # check for exit
+        if (change != -999):
+            
+            # print total change between years
+            print("total percent change from " + str(original) + " to " + str(new_number) + "is: " + str(change))
+            print("$" + str(original_amount) + " from " + str(original) + " is worth about $" + str(total_dollar) + " in " + str(new_number))
+
+        else:
+
+            #print an error
+            print("Make sure new_number enter a year between 1960 and 2019")
+
+        # keep reading input
+        original = input("Enter a year: ")
+
+
+main()
