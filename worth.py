5aa2759f6a18521a7edb7f5ece5aa2ef2085f3c3
Ryan Ostrander <ryanostrander99@gmail.com>
2019-10-18 17:00:08 -0500

    expanded data set

--- worth.py
+++ worth.py
@@ -12,12 +12,12 @@ def inflation_reader(dict):
 
 # calculate total percent change from one year to another
 # includes error checking for year boundary condition
-def calculate_pct_change(orig, inc, dict):
+def calculate_pct_change(orig, inc, dct):
 
     # boundary condition
-    if ((orig >= 1960 or orig <= 2019) and (inc >= 1960 or inc <= 2019)):
-        _original = dict[str(orig)]
-        increase = dict[str(inc)] - _original
+    if ((orig >= 1800 or orig <= 2019) and (inc >= 1800 or inc <= 2019)):
+        _original = dct[str(orig)]
+        increase = dct[str(inc)] - _original
         change = round((increase/_original) * 100.0, 2)
     else:
         return -999
@@ -37,26 +37,26 @@ def main():
 
         # prompt for more information
         new_number = input("Enter another year: ")
-        original_amount = input("Enter amount: ")
+        original_amount = input("Enter amount: $")
 
         # calculate total percent change
         change = calculate_pct_change(original, new_number, inflation_dict)
         decimal_change = change / 100.0
 
         # calculate total change between years for a given amount
-        total_dollar = (original_amount*decimal_change) + original_amount
+        total_dollar = round((original_amount*decimal_change) + original_amount, 2)
 
         # check for exit
         if (change != -999):
             
             # print total change between years
-            print("total percent change from " + str(original) + " to " + str(new_number) + "is: " + str(change))
+            print("Total percent change from " + str(original) + " to " + str(new_number) + " is: " + str(change) + "%")
             print("$" + str(original_amount) + " from " + str(original) + " is worth about $" + str(total_dollar) + " in " + str(new_number))
 
         else:
 
             #print an error
-            print("Make sure new_number enter a year between 1960 and 2019")
+            print("Make sure new_number enter a year between 1800 and 2019")
 
         # keep reading input
         original = input("Enter a year: ")
